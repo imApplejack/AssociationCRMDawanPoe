677b7e04ac9466c517b96241159929b2b0dd6361
Eric <eric.guilbaud@gmail.com>
2022-11-24 15:41:29 +0100

    maj

---
+++ Persistance/AbstractRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssociationCRMDawanPoe.Persistance
+{
+    public class AbstractRepository
+    {
+        public string ConnexionString { get; set; }
+
+        public AbstractRepository(string connexionString)
+        {
+            ConnexionString = connexionString;
+        }
+    }
+}
