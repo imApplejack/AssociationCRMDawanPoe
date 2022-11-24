e7a70b5c6d285083a29453616a79ed403d87b79a
Arnaud <arnaudandjemien@gmail.com>
2022-11-24 16:11:07 +0100

    maj Persistance Producr

--- Service/IProductService.cs
+++ Service/IProductService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AssociationCRMDawanPoe.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace AssociationCRMDawanPoe.Service
 {
     public interface IProductService
     {
+        public List<Product> getAllProduct();
+
     }
 }
